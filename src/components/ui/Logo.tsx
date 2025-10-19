c83285920b4ac1f6f7c23d1b4b6684319d5f84d2
VibeOS <andrew@aisimple.co>
2025-10-19 03:27:59 +0000

    Updated index.html

--- src/components/ui/Logo.tsx
+++ src/components/ui/Logo.tsx
@@ -21,14 +21,14 @@ const Logo = ({ size = 'md', className = '' }: LogoProps) => {
 
   return (
     <div className={`${sizeClasses[size]} bg-black rounded-full flex items-center justify-center ${className}`}>
-      <svg 
-        xmlns="http://www.w3.org/2000/svg" 
-        height={iconSizes[size]} 
-        viewBox="0 -960 960 960" 
-        width={iconSizes[size]} 
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        height={iconSizes[size]}
+        viewBox="0 -960 960 960"
+        width={iconSizes[size]}
         fill="#FFFFFF"
       >
-        <path d="M480-80q-33 0-56.5-23.5T400-160h160q0 33-23.5 56.5T480-80ZM320-200v-80h320v80H320Zm10-120q-69-41-109.5-110T180-580q0-125 87.5-212.5T480-880q125 0 212.5 87.5T780-580q0 81-40.5 150T630-320H330Zm24-80h252q45-32 69.5-79T700-580q0-92-64-156t-156-64q-92 0-156 64t-64 156q0 54 24.5 101t69.5 79Zm126 0Z" />
+        <path d="M160-160q-33 0-56.5-23.5T80-240v-480q0-33 23.5-56.5T160-800h640q33 0 56.5 23.5T880-720v480q0 33-23.5 56.5T800-160H160Zm0-80h640v-480H160v480Zm80-80h480v-80H240v80Zm0-160h480v-80H240v80Zm0-160h480v-80H240v80Z" />
       </svg>
     </div>
   );
